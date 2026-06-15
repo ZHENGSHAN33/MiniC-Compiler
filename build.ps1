4aca4f02cf7eff828d85356dc6e762822938256b
starry-lars <2519392703@qq.com>
2026-06-15 21:10:37 +0800

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -1,8 +1,5 @@
 $ErrorActionPreference = "Stop"
 
-<<<<<<< HEAD
-g++ -std=c++17 -Wall -Wextra -O2 -Iinclude src/main.cpp -o compiler.exe
-=======
 g++ -std=c++17 -Wall -Wextra -O2 -Iinclude src/main.cpp src/lexer.cpp -o compiler.exe
->>>>>>> 7cf4835 (feat(lexer):完成词法分析器功能)
+
 Write-Host "build ok: compiler.exe"
